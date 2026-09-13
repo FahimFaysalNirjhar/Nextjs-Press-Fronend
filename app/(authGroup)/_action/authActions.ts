3fe6d2bdf7553e992c371a9e17dd1933635a7c72
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-09-13 21:00:24 +0600

    cookie is set in cookies successfully

--- app/(authGroup)/_action/authActions.ts
+++ app/(authGroup)/_action/authActions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 type LoginState = {
   success: boolean;
   statusCode: number;
@@ -77,7 +79,23 @@ export const loginAction = async (
   });
 
   const result = await res.json();
-  console.log(result);
+
+  if (result.success) {
+    const cookieStore = await cookies();
+
+    cookieStore.set("accessToken", result.data.accessToken, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24,
+      sameSite: "lax",
+    });
+
+    cookieStore.set("refreshToken", result.data.refreshToken, {
+      httpOnly: true,
+      maxAge: 60 * 60 * 24 * 7,
+      sameSite: "lax",
+    });
+  }
+
   return result;
 };
 
